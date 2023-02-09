f352075014de316f1ca5b3cfb39aa63f5870ec39
Abhishek <Abhismart098@gmail.com>
2023-02-09 11:36:32 +0530

    Runtime Error

---
+++ src/main/java/com/first/connectfour/Controller.java
@@ -0,0 +1,75 @@
+package com.first.connector;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	private static final int COLUMNS = 7;
+	private static final int ROWS = 6;
+	private static final int CIRCLE_DIAMETER = 80;
+	private static final String discColor1 = "#24303E";
+	private static final String discColor2 = "#4CAA88";
+
+	private static String PLAYER_ONE = "Player One";
+	private static String PLAYER_TWO = "Player Two";
+
+
+	private boolean isPlayerOneTurn = true;
+
+	@FXML
+	public GridPane rootGridPane;
+
+	@FXML
+	public Pane insertedDiscsPane;
+
+	@FXML
+	public Label playerNameLabel;
+
+	public void createPlayground() {
+
+		Shape rectangleWithHoles = createGameStructuralGrid();
+		rootGridPane.add(rectangleWithHoles, 0, 1);
+	}
+
+	private Shape createGameStructuralGrid() {
+
+		Shape rectangleWithHoles = new Rectangle((COLUMNS + 1) * CIRCLE_DIAMETER, (ROWS + 1) * CIRCLE_DIAMETER);
+
+		for (int row = 0; row < ROWS; row++) {
+
+			for (int col = 0; col < COLUMNS; col++) {
+				Circle circle = new Circle();
+				circle.setRadius(CIRCLE_DIAMETER / 2);
+				circle.setCenterX(CIRCLE_DIAMETER / 2);
+				circle.setCenterY(CIRCLE_DIAMETER / 2);
+
+				circle.setTranslateX(col * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
+				circle.setTranslateY(row * (CIRCLE_DIAMETER + 5) + CIRCLE_DIAMETER / 4);
+
+				rectangleWithHoles = Shape.subtract(rectangleWithHoles, circle);
+			}
+		}
+
+		rectangleWithHoles.setFill(Color.WHITE);
+
+		return rectangleWithHoles;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+	}
