cff317c61b953364c432262be2fce159e463f25e
Abhishek <Abhismart098@gmail.com>
2023-02-16 20:07:25 +0530

    Updated a code

--- src/main/java/com/first/connectfour/Controller2.java
+++ src/main/java/com/first/connectfour/Controller2.java
@@ -1,2 +1,25 @@
-package com.first.connectfour;public class Controller2 {
+package com.first.connectfour;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller2  implements Initializable {
+
+    @FXML
+    public GridPane rootGridPane;
+
+    @FXML
+    public Pane insertedDiscsPane;
+
+    @FXML
+    public Label playerNameLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
